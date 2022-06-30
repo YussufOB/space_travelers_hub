793f0f7a0746e10e11f3ed9bb10b56aa3074f5a0
richemonday@gmail.com <richemonday@gmail.com>
2022-06-30 16:51:51 +0100

    styling

--- src/components/pages/NavLink.js
+++ src/components/pages/NavLink.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { spaceIcon } from '../assets/spaceIcon.png';
+import spaceIcon from '../assets/spaceIcon.png';
+import './NavLink.css';
 
 const NavBar = () => (
   <nav>
